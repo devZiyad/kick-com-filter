d305cb7cb8c7a2556e6da90d4655311580ce33b1
devZiyad <ziyad.s.alhajri@gmail.com>
2024-08-22 15:16:45 +0300

    Initial tampermonkey script structure

---
+++ main.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Intercept and Filter Kick.com API Calls
+// @namespace    http://tampermonkey.net/
+// @version      0.0
+// @description  Intercept API calls to Kick.com and filter out blacklisted streamers
+// @author       devZiyad
+// @match        *://kick.com/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+	'use strict';
+})();
