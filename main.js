27be4924758fc7ababf1d1607ec9110407219919
devZiyad <ziyad.s.alhajri@gmail.com>
2024-08-22 16:18:48 +0300

    changed script name, and added stream filter by username

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Intercept and Filter Kick.com API Calls
+// @name         Filter Kick.com
 // @namespace    http://tampermonkey.net/
-// @version      0.0
+// @version      1.0
 // @description  Intercept API calls to Kick.com and filter out blacklisted streamers
 // @author       devZiyad
 // @match        *://kick.com/*
@@ -9,6 +9,123 @@
 // @run-at       document-start
 // ==/UserScript==
 
+
+const blacklist = {
+	"streamer": [],
+};
+
+const api_urls = [
+	"/stream/featured-livestreams/en",
+	"/stream/livestreams/en"
+];
+
 (function() {
 	'use strict';
+
+	console.log("Running kick.com filter script");	
+
+	// Override fetch calls
+	const originalFetch = window.fetch;
+	window.fetch = function(url, options) {
+		console.log(`Intercepted fetch request to: ${url}`);
+		if (includesAny(url, api_urls)) {
+			return originalFetch(url, options).then(response => {
+				console.log("Original fetch response received");
+
+				// Clone response to modify
+				var clonedResponse = response.clone();
+
+				return clonedResponse.json().then(responseJSON => {
+					console.log("Original fetch response JSON", responseJSON);
+
+					// Filter responseJSON
+					if (responseJSON.data) {
+						responseJSON.data = filterData(responseJSON.data);
+					}
+
+					// Create new response with filtered data
+					const filteredResponse = new Response(JSON.stringify(responseJSON), {
+						status: response.status,
+						statusText: response.statusText,
+						headers: response.headers
+					});
+					console.log("Filtered fetch response JSON: ", responseJSON);
+					return filteredResponse;
+				});
+			});
+		} else {
+			return originalFetch(url, options);
+		}
+	};
+
+	// Override XMLHttpRequest calls
+	const originalXHR = window.XMLHttpRequest;
+    window.XMLHttpRequest = function() {
+        const xhr = new originalXHR();
+
+        const originalOpen = xhr.open;
+        const originalSend = xhr.send;
+
+        xhr.addEventListener('readystatechange', function() {
+            if (xhr.readyState === 4 && includesAny(xhr.responseURL, api_urls)) {
+                console.log(`Intercepted XMLHttpRequest to: ${xhr.responseURL}`);
+                const originalResponse = xhr.responseText;
+                try {
+                    console.log('Original XMLHttpRequest response:', originalResponse);
+                    var responseJSON = JSON.parse(originalResponse);
+                    if (responseJSON.data) {
+						responseJSON.data = filterData(responseJSON.data);
+                    }
+
+                    // Define a writable `responseText` property
+                    Object.defineProperty(xhr, 'responseText', {
+                        writable: true
+                    });
+
+                    // Update the internal responseText to the filtered data
+                    xhr.responseText = JSON.stringify(responseJSON);
+
+                    console.log('Filtered XMLHttpRequest response:', xhr.responseText);
+                } catch (e) {
+                    console.error('Failed to parse JSON response:', e);
+                }
+            }
+        });
+
+        return xhr;
+    };
 })();
+
+
+// Utility Functions
+
+function includesAny(str, arr) {
+	return arr.some(subStr => {
+		return str.includes(subStr)
+	});
+}
+
+function filterData(data) {
+	var filters = Object.keys(blacklist);
+	filters.forEach(filter => {
+		switch (filter) {
+			case "streamer":
+				var filterFunction = filterStreamers;
+				break;
+		}
+
+		data = filterFunction(data, blacklist[filter]);
+	});
+
+	return data;
+}
+
+function filterStreamers(data, streamerList) {
+	return data.filter(stream => {
+		const isBlacklisted = streamerList.includes(stream.channel.user.username);
+		if (isBlacklisted) {
+			console.log(`Filtering out blacklisted streamer: ${stream.channel.user.username}`);
+		}
+		return !isBlacklisted;
+	});
+}
