921ba47b922f763abe97680552b13e06416c1273
devZiyad <ziyad.s.alhajri@gmail.com>
2024-08-26 11:44:00 +0300

    removed fetch function override and unused lines

--- main.js
+++ main.js
@@ -24,78 +24,41 @@ const api_urls = [
 (function() {
 	'use strict';
 
-	console.log("Running kick.com filter script");	
-
-	// Override fetch calls
-	const originalFetch = window.fetch;
-	window.fetch = function(url, options) {
-		console.log(`Intercepted fetch request to: ${url}`);
-		if (includesAny(url, api_urls)) {
-			return originalFetch(url, options).then(response => {
-				console.log("Original fetch response received");
-
-				// Clone response to modify
-				var clonedResponse = response.clone();
-
-				return clonedResponse.json().then(responseJSON => {
-					console.log("Original fetch response JSON", responseJSON);
-
-					// Filter responseJSON
-					if (responseJSON.data) {
-						responseJSON.data = filterData(responseJSON.data);
-					}
-
-					// Create new response with filtered data
-					const filteredResponse = new Response(JSON.stringify(responseJSON), {
-						status: response.status,
-						statusText: response.statusText,
-						headers: response.headers
-					});
-					console.log("Filtered fetch response JSON: ", responseJSON);
-					return filteredResponse;
-				});
-			});
-		} else {
-			return originalFetch(url, options);
-		}
-	};
+	console.log("Running kick.com filter script");
 
 	// Override XMLHttpRequest calls
 	const originalXHR = window.XMLHttpRequest;
-    window.XMLHttpRequest = function() {
-        const xhr = new originalXHR();
+	window.XMLHttpRequest = function() {
+		const xhr = new originalXHR();
 
-        const originalOpen = xhr.open;
-        const originalSend = xhr.send;
+		xhr.addEventListener('readystatechange', function() {
+		    if (xhr.readyState === 4 && includesAny(xhr.responseURL, api_urls)) {
+			console.log(`Intercepted XMLHttpRequest to: ${xhr.responseURL}`);
+			const originalResponse = xhr.responseText;
+			try {
+			    console.log('Original XMLHttpRequest response:', originalResponse);
+			    var responseJSON = JSON.parse(originalResponse);
+			    if (responseJSON.data) {
+							responseJSON.data = filterData(responseJSON.data);
+			    }
 
-        xhr.addEventListener('readystatechange', function() {
-            if (xhr.readyState === 4 && includesAny(xhr.responseURL, api_urls)) {
-                console.log(`Intercepted XMLHttpRequest to: ${xhr.responseURL}`);
-                const originalResponse = xhr.responseText;
-                try {
-                    console.log('Original XMLHttpRequest response:', originalResponse);
-                    var responseJSON = JSON.parse(originalResponse);
-                    if (responseJSON.data) {
-						responseJSON.data = filterData(responseJSON.data);
-                    }
+			    // Define a writable `responseText` property
+			    Object.defineProperty(xhr, 'responseText', {
+				writable: true
+			    });
 
-                    // Define a writable `responseText` property
-                    Object.defineProperty(xhr, 'responseText', {
-                        writable: true
-                    });
+			    // Update the internal responseText to the filtered data
+			    xhr.responseText = JSON.stringify(responseJSON);
 
-                    // Update the internal responseText to the filtered data
-                    xhr.responseText = JSON.stringify(responseJSON);
+			    console.log('Filtered XMLHttpRequest response:', xhr.responseText);
+			} catch (e) {
+			    console.error('Failed to parse JSON response:', e);
+			}
+		    }
+		});
 
-                    console.log('Filtered XMLHttpRequest response:', xhr.responseText);
-                } catch (e) {
-                    console.error('Failed to parse JSON response:', e);
-                }
-            }
-        });
-
-        return xhr;
-    };
+		return xhr;
+	    };
 })();
 
 
